936d3a32f89e559fdc2b908933c4326e1039272e
Clovis Durand <cd.clovel19@gmail.com>
2019-11-07 09:51:54 +0100

    [#6] Added a class to parse and hold HTTP request data
    Signed-off-by: Clovis Durand <cd.clovel19@gmail.com>

---
+++ research/web-example/HttpRequest.hxx
@@ -0,0 +1,48 @@
+/** 
+ * Copyright Clovis Durand
+ * Let There Be Light project
+ * 
+ * @file HttpRequest.hxx
+ */
+
+/* Includes -------------------------------------------- */
+#include <string>
+
+/* Defines --------------------------------------------- */
+
+/* Type definitions ------------------------------------ */
+
+/* Variable declarations ------------------------------- */
+
+/* Functions ------------------------------------------- */
+
+/* Class definition ------------------------------------ */
+class HttpRequest {
+    public:
+        /* Constructors */
+        HttpRequest();
+
+        /* Destructors */
+        virtual ~HttpRequest();
+
+        /* Request parser */
+        int parseRequest(const std::string &pRequest);
+
+        /* Getters */
+        std::string request(void) const;
+        std::string method(void) const;
+        std::string URL(void) const;
+        std::string shortURL(void) const;
+        std::string query(void) const;
+        std::string httpVersion(void) const;
+
+    protected:
+        std::string mRequestStr;
+        
+        std::string mMethod;
+        std::string mURL;
+        std::string mShortURL;
+        std::string mQuery;
+        std::string mHttpVersion;
+    private:
+};
