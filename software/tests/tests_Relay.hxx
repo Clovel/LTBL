ef8e205fc91da919edba7eac3cfa4171199e7013
Clovis Durand <cd.clovel19@gmail.com>
2019-11-20 19:54:32 +0100

    [#13] Started Relay class UTs
    Signed-off-by: Clovis Durand <cd.clovel19@gmail.com>

---
+++ software/tests/tests_Relay.hxx
@@ -0,0 +1,10 @@
+/**
+ * @brief UTs for the Relay class
+ */
+
+/* Tests ----------------------------------------------- */
+int test_Relay_isOn(void);
+int test_Relay_switchState(void);
+int test_Relay_turnOn(void);
+int test_Relay_turnOff(void);
+int test_Relay_setMode(void);
---
+++ software/tests/tests_main.cxx
@@ -0,0 +1,48 @@
+/**
+ * @brief Main testing file
+ */
+
+/* Includes -------------------------------------------- */
+/* Gaussian */
+#include "tests_Relay.hxx"
+
+/* System */
+#include <iostream>
+
+/* C System */
+#include <cstring>
+
+/* Support functions ----------------------------------- */
+static void print_usage(const char * const pProgName)
+{
+    std::cout << "[USAGE] " << pProgName << " <test#>" << std::endl;
+}
+
+/* Main ------------------------------------------------ */
+int main(const int argc, const char * const * const argv) {
+    /* Test function initialization */
+    int32_t lTestNum;
+    int16_t lResult = EXIT_SUCCESS;
+
+    if ((2 > argc) || (0 == std::strcmp(argv[1], "--help"))) {
+        print_usage(argv[0]);
+        return EXIT_FAILURE;
+    }
+
+    lTestNum = strtol(argv[1], NULL, 10);
+    std::cout << "[TEST ] Executing test #" << lTestNum << std::endl;
+
+    /* Executing test */
+    switch (lTestNum) {
+        case 0:
+            lResult = test_Relay_isOn()
+            break;
+        default:
+            std::cout << "[INFO ] test #" << lTestNum << " not available" << std::endl;
+            fflush(stdout);
+            lResult = EXIT_FAILURE;
+            break;
+    }
+
+    return lResult;
+}
