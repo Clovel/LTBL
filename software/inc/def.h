83495d37dc5da0d209627474b06594392ae17811
Clovis Durand <cd.clovel19@gmail.com>
2019-12-13 13:18:49 +0100

    Added def.h to contain general defines, settings & definitions
    Signed-off-by: Clovis Durand <cd.clovel19@gmail.com>

---
+++ software/inc/def.h
@@ -0,0 +1,11 @@
+/** 
+ * Let There Be Light project
+ * 
+ * @file def.h
+ * 
+ * @brief This file contains general project settings, 
+ * type definitions & defines.
+ */
+
+/* Defines --------------------------------------------- */
+#define LOG_BAUDRATE 115200U
