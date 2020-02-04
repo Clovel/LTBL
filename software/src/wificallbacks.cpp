7c2f2e7082ad030cd6a3b6f6c9e33add8010b8d3
Clovis Durand <cd.clovel19@gmail.com>
2020-02-04 20:47:33 +0100

    Use of Logger in wificallbacks.cpp
    Signed-off-by: Clovis Durand <cd.clovel19@gmail.com>

--- software/src/wificallbacks.cpp
+++ software/src/wificallbacks.cpp
@@ -7,26 +7,31 @@
  */
 
 /* Includes -------------------------------------------- */
+#include "Logger.hpp"
+
 #include <Arduino.h>
 
 #include <WiFiManager.h> /* https://github.com/tzapu/WiFiManager WiFi Configuration Magic */
 #include <ESP8266WiFi.h> /* ESP8266 Core WiFi Library */
+
+/* External variables ---------------------------------- */
+extern Logger *gLogger;
 
 /* Static variables ------------------------------------ */
 static bool shouldSaveConfig = false; /* Config save flag */
 
 /* WiFiManager callbacks ------------------------------- */
 void configModeCallback(WiFiManager *myWiFiManager) {
-    Serial.println("[BOOT ] <WiFiManager> Entered config mode");
-    Serial.println(WiFi.softAPIP());
+    *gLogger << "[BOOT ] <WiFiManager> Entered config mode" << endlog;
+    *gLogger << WiFi.softAPIP().toString() << endlog;
 
     /* Print SSID */
-    Serial.print("[BOOT ] <WiFiManager> SSID : ");
-    Serial.println(myWiFiManager->getConfigPortalSSID());
+    *gLogger << "[BOOT ] <WiFiManager> SSID : ";
+    *gLogger << myWiFiManager->getConfigPortalSSID() << endlog;
 }
 
 /* Callback notifying us of the need to save config */
 void saveConfigCallback (void) {
-    Serial.println("Should save config");
+    *gLogger << "Should save config" << endlog;
     shouldSaveConfig = true;
 }
