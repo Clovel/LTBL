4460de8924b9b5ad2241c80cbe46e96b787ad75a
Clovis Durand <cd.clovel19@gmail.com>
2020-02-04 23:39:09 +0100

    Moved restMethod_t from webtools.hpp to HttpRequest.hpp
    Signed-off-by: Clovis Durand <cd.clovel19@gmail.com>

--- software/inc/HttpRequest.hpp
+++ software/inc/HttpRequest.hpp
@@ -13,6 +13,17 @@
 /* Defines --------------------------------------------- */
 
 /* Type definitions ------------------------------------ */
+typedef enum _restMethods {
+    REST_UNKNOWN = 0,
+    REST_INDEX, /* Shows the index page */
+    REST_GET, /* GET is used to request data from a specified resource. */
+    REST_POST, /* POST is used to send data to a server to create/update a resource. */
+    REST_PUT, /* PUT is used to send data to a server to create/update a resource. */
+    REST_HEAD, /* HEAD is almost identical to GET, but without the response body. */
+    REST_DELETE, /* The DELETE method deletes the specified resource. */
+    REST_PATCH,
+    REST_OPTIONS /* The OPTIONS method describes the communication options for the target resource. */
+} restMethod_t;
 
 /* Variable declarations ------------------------------- */
 
